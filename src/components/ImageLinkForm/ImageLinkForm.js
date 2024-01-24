9b591b222860fa985fd1d21fad6be1af7a0f5ac5
AlejandroTVG <AlejandroCR@thevitalgroup.org>
2024-01-24 17:44:32 +0000

    set a default URL for ininitial testing

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -13,6 +13,9 @@ export default function ImageLinkForm({ onImageURLChange, onButtonSubmit }) {
             className="f4 pa2 w-70 center"
             type="text"
             onChange={onImageURLChange}
+            defaultValue={
+              "https://img.freepik.com/foto-gratis/vista-frontal-retrato-joven-bella-mujer-despues-spa_176420-8732.jpg?w=1380&t=st=1706072715~exp=1706073315~hmac=ed2280004a93b4dbbf211aae8385020761e6083b51ddc850dcf8815e2fa1863f"
+            }
           />
           <button
             onClick={onButtonSubmit}
